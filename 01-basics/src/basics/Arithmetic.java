a82c6447fdf3bf6587201b26c6c8b31c5dbbb7d2
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-12 22:10:10 +0530

    Added Arithmetic operations program

---
+++ 01-basics/src/basics/Arithmetic.java
@@ -0,0 +1,25 @@
+package basics;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+        // int DataType
+
+        // int x = 5;
+        // int y = 2;
+        // System.out.println(x + y);
+        // System.out.println(x - y);
+        // System.out.println(x * y);
+        // System.out.println(x / y);
+        // System.out.println(x % y);
+
+        // double DataTypes
+
+        double x = 5;
+        double y = 2;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+    }
+}

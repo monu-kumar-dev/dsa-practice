c924e49e0f42bde4128fd5336b78eaf38d630767
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-12 22:10:55 +0530

    Added variables example

---
+++ 01-basics/src/basics/Variable.java
@@ -0,0 +1,21 @@
+package basics;
+
+public class Variable {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        int y;
+        y = 20;
+        System.out.println(y);
+        System.out.println(x * y);
+
+        int a;
+        a = 25;
+        System.out.println(a);
+        a = a - 5;
+        System.out.println(a);
+        a = a * 10;
+        System.out.println(a);
+
+    }
+}

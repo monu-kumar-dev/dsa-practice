5b4426611f683e236217e879d9a2cb5339f34abd
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-12 17:06:13 +0530

    Add explanation of println vs newline in Java

--- 01-basics/src/basics/Hello.java
+++ 01-basics/src/basics/Hello.java
@@ -2,6 +2,13 @@ package basics;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello Monu!");
+
+        // Type - 1 (using println)
+        System.out.print("Hello Monu!");
+        System.out.println("Hello sohan!");
+        System.out.print("Hello soh0an!");
+
+        // Type - 2 (using \n)
+        System.out.print("hello Monu \n sohan jee");
     }
 }

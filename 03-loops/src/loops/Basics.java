009046ed9c856de0868da8fe41ee93c5674fa564
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-14 18:15:53 +0530

    Add basic for-loop example in Java

---
+++ 03-loops/src/loops/Basics.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class Basics {
+    public static void main(String[] args) {
+        // System.out.println("Amir HAtes Nikita");
+        // System.out.println("Amir HAtes Nikita");
+        // System.out.println("Amir HAtes Nikita");
+        // System.out.println("Amir HAtes Nikita");
+
+        // Shorthand
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Amir HAtes Nikita");
+        }
+    }
+}

9a5a95132bd8ed427530472d3e2aad2b22a70c26
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-14 21:55:27 +0530

    Add program to print numbers from 1 to 100 using for loop

---
+++ 03-loops/src/loops/PrintNum.java
@@ -0,0 +1,10 @@
+// Ques: Print numbers from 1 to 100
+package loops;
+
+public class PrintNum {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.print(i + 1 + " ");
+        }
+    }
+}

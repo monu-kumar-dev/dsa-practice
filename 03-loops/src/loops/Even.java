3132292be09be1167068e386656e4bed1e09c415
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-14 21:57:47 +0530

    Add program to print even numbers from 1 to 100 using for loop

---
+++ 03-loops/src/loops/Even.java
@@ -0,0 +1,13 @@
+// Ques: Print all even numbers from 1 to 100
+
+package loops;
+
+public class Even {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

de35a3ac813edf7f05a554e2ff437edea04bd7bd
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-16 23:40:45 +0530

    Add Floyd's triangle number pattern program

---
+++ 04-patterns/src/patterns/FloydsTriangle.java
@@ -0,0 +1,30 @@
+// Ques: Print the given pattern
+// Enter n: 4
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+
+package patterns;
+
+import java.util.Scanner;
+
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int count = 1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+}

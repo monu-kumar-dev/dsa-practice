ea977f463612d5fa15fa8c8d26ca2f5ddfcf11dc
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-16 23:12:40 +0530

    Add star plus pattern for odd-sized grid

---
+++ 04-patterns/src/patterns/StarPlus.java
@@ -0,0 +1,41 @@
+// Ques: Print the given pattern
+//     *     
+//     *     
+// * * * * * 
+//     *     
+//     *     
+
+
+package patterns;
+
+import java.util.Scanner;
+
+public class StarPlus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n(always odd num): ");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0) {
+            System.out.println("Please enter odd num");
+            sc.close();
+            return;
+        }
+
+        int mid = (n / 2) + 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (mid == i || mid == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+
+    }
+}

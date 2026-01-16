860c438ec6cea32bae14d1217546e9cc6fba443b
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-16 18:54:32 +0530

    Add alphabet square pattern program using nested loops

---
+++ 04-patterns/src/patterns/AlphabetSquare.java
@@ -0,0 +1,29 @@
+// Ques: Print the given pattern
+// Enter n: 4
+// A B C D 
+// A B C D 
+// A B C D 
+// A B C D 
+
+package patterns;
+
+import java.util.Scanner;
+
+public class AlphabetSquare {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            char ch = 'A';
+            for (int j = 1; j <= n; j++) {
+                System.out.print(ch + " ");
+                ch++;
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

f129083e6cb1e2f164aa2f81a095253a6a9968e9
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-17 22:34:04 +0530

    Implement centered star pyramid pattern using nested loops

---
+++ 04-patterns/src/patterns/Pyramid.java
@@ -0,0 +1,29 @@
+// Ques: Print the given pattern
+// Enter the num: 4
+//       * 
+//     * * * 
+//   * * * * *
+// * * * * * * *
+
+package patterns;
+
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the num: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}

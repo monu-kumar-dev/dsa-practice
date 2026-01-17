07d7bceaf56f2bff1a7712766f767aaf0e88100a
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-17 22:49:15 +0530

    Add Type-2 pyramid pattern using space and star counters

--- 04-patterns/src/patterns/Pyramid.java
+++ 04-patterns/src/patterns/Pyramid.java
@@ -5,6 +5,9 @@
 //   * * * * *
 // * * * * * * *
 
+// 🥇 Verdict
+// DSA + Interview → Type-2 ✅
+
 package patterns;
 
 import java.util.Scanner;
@@ -15,13 +18,32 @@ public class Pyramid {
         System.out.print("Enter the num: ");
         int n = sc.nextInt();
 
+        // Type - 1
+
+        // for (int i = 1; i <= n; i++) {
+        // for (int j = 1; j <= n - i; j++) {
+        // System.out.print(" ");
+        // }
+        // for (int k = 1; k <= 2 * i - 1; k++) {
+        // System.out.print("* ");
+        // }
+        // System.out.println();
+        // }
+        // sc.close();
+
+        // Type - 2
+        int nsp = n - 1;
+        int nst = 1;
+
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
+            for (int j = 1; j <= nsp; j++) {
                 System.out.print("  ");
             }
-            for (int k = 1; k <= 2 * i - 1; k++) {
+            for (int k = 1; k <= nst; k++) {
                 System.out.print("* ");
             }
+            nsp--;
+            nst += 2;
             System.out.println();
         }
         sc.close();

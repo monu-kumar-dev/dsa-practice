cdb111bab38cb6da1810f8696a5cdf2f6544c9e2
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-18 20:24:44 +0530

    Add number spiral pattern program using nested loops

--- 04-patterns/src/patterns/NumberSpiral.java
+++ 04-patterns/src/patterns/NumberSpiral.java
@@ -1,10 +1,12 @@
 // Ques: Print the given pattern
-// Enter the num: 5
-// * * * * * * * * * 
-// * * * *   * * * * 
-// * * *       * * *
-// * *           * *
-// *               *
+// Number Spiral, n = 4
+// 1 1 1 1 1 1 1
+// 1 2 2 2 2 2 1
+// 1 2 3 3 3 2 1
+// 1 2 3 4 3 2 1
+// 1 2 3 3 3 2 1
+// 1 2 3 3 3 2 1
+// 1 1 1 1 1 1 1
 
 package patterns;
 
@@ -16,7 +18,21 @@ public class NumberSpiral {
         System.out.print("Enter the num: ");
         int n = sc.nextInt();
 
-        
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            for (int j = 1; j <= 2 * n - 1; j++) {
 
+                int a = i;
+                int b = j;
+
+                if (i > n)
+                    a = 2 * n - i;
+                if (j > n)
+                    b = 2 * n - j;
+
+                System.out.print(Math.min(a, b) + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
     }
 }

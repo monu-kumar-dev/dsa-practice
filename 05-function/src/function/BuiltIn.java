46ff39834d54e7dc9e436a78682aa4e6e858845d
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-18 20:59:35 +0530

    Add demo of Java Math built-in functions

---
+++ 05-function/src/function/BuiltIn.java
@@ -0,0 +1,23 @@
+package function;
+
+public class BuiltIn {
+    public static void main(String[] args) {
+
+        // Math. wale function
+
+        // double x = Math.sqrt(4);
+        // System.out.print(x);
+
+        System.out.println("Square root of 10: " + Math.sqrt(10));
+        System.out.println("Cube root of 125: " + Math.cbrt(125));
+        System.out.println("Value of PI: " + Math.PI);
+        System.out.println("Absolute of -76: " + Math.abs(-76));
+        System.out.println("Floor of 6.8: " + Math.floor(6.8));
+        System.out.println("Floor of -6.8: " + Math.floor(-6.8));
+        System.out.println("Ceil of -6.8: " + Math.ceil(-6.8));
+        System.out.println("Ceil of 6.8: " + Math.ceil(6.8));
+        System.out.println("Min of 6.8 and 8.7: " + Math.min(6.8, 8.7));
+        System.out.println("Max of 6.8 and 8.7: " + Math.max(6.8, 8.7));
+
+    }
+}

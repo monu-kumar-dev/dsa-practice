7a4bf6513974f58983c6396912473e23a812e8e1
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-19 19:30:25 +0530

    Add Syntax class with simple function call example

---
+++ 05-function/src/function/Syntax.java
@@ -0,0 +1,14 @@
+package function;
+
+public class Syntax {
+    public static void monu() {
+        System.out.println("Monu");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Rahul");
+        monu();
+        System.out.println("Sonu");
+        monu();
+    }
+}

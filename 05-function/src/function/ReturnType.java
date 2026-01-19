c157535792291662594a23978145660a95d2f65d
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-19 20:12:01 +0530

    Add ReturnType example demonstrating return statement behavior

---
+++ 05-function/src/function/ReturnType.java
@@ -0,0 +1,16 @@
+package function;
+
+public class ReturnType {
+    public static int prasun(int a) {
+        System.out.println("banu");
+        if (a > 0)
+            return 5;
+        else
+            return 10;
+    }
+
+    public static void main(String[] args) {
+        int x = prasun(7);
+        System.out.println(3 + x);
+    }
+}

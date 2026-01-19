30d35101f30576642e98180044eb21b5b6a93434
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-19 19:40:24 +0530

    Add GuessOutput2 program to practice nested method execution order

---
+++ 05-function/src/function/GuessOutput2.java
@@ -0,0 +1,23 @@
+package function;
+
+public class GuessOutput2 {
+    public static void sharavan() {
+        karan();
+        System.out.println("Khusi");
+    }
+
+    public static void main(String[] args) {
+        rivamshi();
+    }
+
+    public static void karan() {
+        System.out.println("vashika");
+    }
+
+    public static void rivamshi() {
+        karan();
+        sharavan();
+        System.out.println("Ajay");
+        sharavan();
+    }
+}

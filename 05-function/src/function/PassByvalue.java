b520fdc4f8e28c13fb3afc7ccec5dcb0349aa0b8
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-19 20:53:59 +0530

    Add example demonstrating call by value in Java

---
+++ 05-function/src/function/PassByvalue.java
@@ -0,0 +1,14 @@
+package function;
+
+public class PassByvalue {
+    public static void change(int x) {
+        x = 10;
+    }
+
+    public static void main(String[] args) {
+        int x = 6;
+        System.out.println(x);
+        change(x);
+        System.out.println(x);
+    }
+}

491b7c2774c823e648c1258fa663a9ae71050ebd
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-19 19:36:09 +0530

    Add GuessOutput program to demonstrate nested function calls

---
+++ 05-function/src/function/GuessOutput.java
@@ -0,0 +1,21 @@
+package function;
+
+public class GuessOutput {
+    public static void main(String[] args) {
+        rivamshi();
+    }
+
+    public static void rivamshi() {
+        System.out.println("Ajay");
+        sharavan();
+    }
+
+    public static void sharavan() {
+        karan();
+        System.out.println("Khusi");
+    }
+
+    public static void karan() {
+        System.out.println("vashika");
+    }
+}

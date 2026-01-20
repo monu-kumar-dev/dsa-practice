99bc72d94d595febdb7c7094c6d0ecd555768002
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 18:33:42 +0530

    Printed array elements using for loop and length property

--- 06-Array/src/array/InputOutput.java
+++ 06-Array/src/array/InputOutput.java
@@ -3,11 +3,15 @@ package array;
 public class InputOutput {
     public static void main(String[] args) {
         int[] arr = { 5, -7, 45, 32, -87 };
-        System.out.println(arr[0]);
-        System.out.println(arr[1]);
-        System.out.println(arr[2]);
-        System.out.println(arr[3]);
-        System.out.println(arr[4]);
+        System.out.println("Length of arr: " + arr.length);
+        // System.out.println(arr[0]);
+        // System.out.println(arr[1]);
+        // System.out.println(arr[2]);
+        // System.out.println(arr[3]);
+        // System.out.println(arr[4]);
 
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }

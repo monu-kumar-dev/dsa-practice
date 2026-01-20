b49dc649830e7ba9d00ccc34322312721dd0d99b
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 18:37:15 +0530

    Demonstrated default values of int array

--- 06-Array/src/array/InputOutput.java
+++ 06-Array/src/array/InputOutput.java
@@ -2,16 +2,23 @@ package array;
 
 public class InputOutput {
     public static void main(String[] args) {
-        int[] arr = { 5, -7, 45, 32, -87 };
-        System.out.println("Length of arr: " + arr.length);
-        // System.out.println(arr[0]);
-        // System.out.println(arr[1]);
-        // System.out.println(arr[2]);
-        // System.out.println(arr[3]);
-        // System.out.println(arr[4]);
+        // int[] arr = { 5, -7, 45, 32, -87 };
+        // System.out.println("Length of arr: " + arr.length);
+        // // System.out.println(arr[0]);
+        // // System.out.println(arr[1]);
+        // // System.out.println(arr[2]);
+        // // System.out.println(arr[3]);
+        // // System.out.println(arr[4]);
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.println(arr[i]);
+        // }
+
+        // Type - 2
+        int[] arr = new int[7];
+        // default values
+        for (int i = 0; i < 7; i++) {
+            System.out.print(arr[i] + " "); // 0 0 0 0 0 0 0
         }
     }
 }

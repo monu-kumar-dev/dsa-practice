6dd879212cc70c3e550261d59ef3abb32dfba157
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 18:19:31 +0530

    Added ArrayBasics program

---
+++ 06-Array/src/array/ArrayBasics.java
@@ -0,0 +1,12 @@
+package array;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+        int[] x = { 2, 4, 3, 6, 8, 1 };
+        // indexing
+        System.out.println(x[3]); // access
+        // Updating elements
+        x[3] = 12;
+        System.out.println(x[3]);
+    }
+}

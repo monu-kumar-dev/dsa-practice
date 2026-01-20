6f99f219dbec8563a31dd76611781ffea7923488
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 21:22:58 +0530

    Demonstrated default values of double and char arrays

--- 06-Array/src/array/ArrayOfOtherDataTypes.java
+++ 06-Array/src/array/ArrayOfOtherDataTypes.java
@@ -7,5 +7,12 @@ public class ArrayOfOtherDataTypes {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " "); // 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
         }
+
+        char[] arr2 = new char[8];
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+            // char[] → default value = '\u0000' (null character)
+            // 👉 That’s why nothing visible will be printed for char array (blank output)
+        }
     }
 }

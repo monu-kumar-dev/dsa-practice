f63474fb3c3e3214b16425d4dd68f634740bc71e
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 18:23:30 +0530

    Added array initialization using new keyword

--- 06-Array/src/array/ArrayBasics.java
+++ 06-Array/src/array/ArrayBasics.java
@@ -2,11 +2,19 @@ package array;
 
 public class ArrayBasics {
     public static void main(String[] args) {
-        int[] x = { 2, 4, 3, 6, 8, 1 };
-        // indexing
-        System.out.println(x[3]); // access
-        // Updating elements
-        x[3] = 12;
-        System.out.println(x[3]);
+
+        // int[] x = { 2, 4, 3, 6, 8, 1 };
+        // // indexing
+        // System.out.println(x[3]); // access
+        // // Updating elements - mutability
+        // x[3] = 12;
+        // System.out.println(x[3]);
+
+        // Type - 2
+        int[] arr = new int[4]; // 4 size ka array
+        arr[0] = 12;
+        arr[1] = 15;
+        arr[2] = 21;
+        arr[3] = 110;
     }
 }

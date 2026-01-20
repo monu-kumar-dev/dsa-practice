a688be9df0609beccdc5d31e479ce71f85f2a502
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 19:22:12 +0530

    Added program to find maximum element in array

---
+++ 06-Array/src/array/Maximum.java
@@ -0,0 +1,15 @@
+package array;
+
+public class Maximum {
+    public static void main(String[] args) {
+        int[] arr = { -12, 32, -43, 76, 87 };
+
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println("Max is: " + max);
+    }
+}

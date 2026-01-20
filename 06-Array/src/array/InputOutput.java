83957e7d89776ed16e465a7d6be5df1e8dfb53a6
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 18:28:44 +0530

    Added InputOutput program to print array elements

---
+++ 06-Array/src/array/InputOutput.java
@@ -0,0 +1,13 @@
+package array;
+
+public class InputOutput {
+    public static void main(String[] args) {
+        int[] arr = { 5, -7, 45, 32, -87 };
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}

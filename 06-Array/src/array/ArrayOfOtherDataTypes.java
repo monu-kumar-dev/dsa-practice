5d491363d9d9fe59caa6f504591b240176e57d39
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-20 21:21:16 +0530

    Demonstrated default values of double array

---
+++ 06-Array/src/array/ArrayOfOtherDataTypes.java
@@ -0,0 +1,11 @@
+package array;
+
+public class ArrayOfOtherDataTypes {
+    public static void main(String[] args) {
+        double[] arr = new double[8];
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " "); // 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
+        }
+    }
+}

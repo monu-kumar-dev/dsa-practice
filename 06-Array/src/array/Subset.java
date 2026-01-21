4fb095961a836a8aaea7958c3a75f5d13b37f687
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-21 22:42:03 +0530

    Implement subset check using sorting and two pointer technique

---
+++ 06-Array/src/array/Subset.java
@@ -0,0 +1,38 @@
+//  Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
+
+package array;
+
+import java.util.Arrays;
+
+public class Subset {
+    public static void main(String[] args) {
+        int[] a = { 11, 7, 31, 13, 21, 3, 7, 3, 1 };
+        int[] b = { 11, 3, 7, 31, 7 };
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        boolean subset = true;
+
+        int i = 0;
+        int j = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] == b[j]) {
+                i++;
+                j++;
+            } else if (a[i] < b[j]) {
+                i++;
+            } else {
+                subset = false;
+                break;
+            }
+        }
+
+        if (j != b.length) {
+            subset = false;
+        }
+
+        System.out.println(subset ? "Yes" : "NO");
+
+    }
+}

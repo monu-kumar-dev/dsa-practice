850a59af5f5ade32f10c80fa9c43821b01e1ab01
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-23 19:34:23 +0530

    Demonstrate foreach loop for array traversal in Java

---
+++ 06-Array/src/array/ForEach.java
@@ -0,0 +1,12 @@
+package array;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] arr = { 5, 8, 2, 6, 9 };
+
+        // Note: We donot perform any operation
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+    }
+}

065fdaf9625ed4d2e9e69a976a5aeb00c46f59ee
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-23 20:04:35 +0530

    Refactor reverse array code using length variable and foreach loop for output

--- 06-Array/src/array/ReverseArray.java
+++ 06-Array/src/array/ReverseArray.java
@@ -4,8 +4,10 @@ public class ReverseArray {
     public static void main(String[] args) {
         int[] arr = { 6, 9, 1, 2, 4, 9 };
 
+        int n = arr.length;
+
         int i = 0;
-        int j = arr.length - 1;
+        int j = n - 1;
 
         while (i < j) {
             int temp = arr[i];
@@ -15,8 +17,14 @@ public class ReverseArray {
             j--;
         }
 
-        for (int p = 0; p < arr.length; p++) {
-            System.out.print(arr[p] + " ");
+        // for (int p = 0; p < n; p++) {
+        // System.out.print(arr[p] + " ");
+        // }
+
+        // using foreach loop
+
+        for (int ele : arr) {
+            System.out.print(ele + " ");
         }
     }
 }

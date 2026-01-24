8d774ba29e392e49ef923edceed0a8ee273d74ab
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-24 21:03:59 +0530

    Rotate array by d positions using reversal algorithm

---
+++ 06-Array/src/array/RotateAtDType_2.java
@@ -0,0 +1,28 @@
+package array;
+
+public class RotateAtDType_2 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5 };
+        int n = arr.length;
+        int d = 3;
+
+        reverse(arr, 0, d - 1);
+        reverse(arr, d, n - 1);
+        reverse(arr, 0, n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}

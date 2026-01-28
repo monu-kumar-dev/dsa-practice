ec6af2e7af63e432e8747937ea3f2f2e1523538a
monu-kumar-dev <monukumar.software@gmail.com>
2026-01-28 23:39:07 +0530

    Implement carry-based addOne logic for digit array

---
+++ 06-Array/src/array/AddOneType2.java
@@ -0,0 +1,35 @@
+package array;
+
+import java.util.Vector;
+import java.util.Collections;
+
+public class AddOneType2 {
+    public static void main(String[] args) {
+        // int[] arr = { 5, 7, 8, 7 };
+        int[] arr2 = { 9, 9, 9 };
+
+        System.out.println(addOne(arr2));
+    }
+
+    public static Vector<Integer> addOne(int[] arr) {
+        int n = arr.length - 1;
+        Vector<Integer> result = new Vector<>();
+        int carry = 1;
+        for (int i = n; i >= 0; i--) {
+            if (arr[i] + carry <= 9) {
+                result.add(arr[i] + carry);
+                carry = 0;
+            } else {
+                result.add(0);
+                carry = 1;
+            }
+        }
+        // if carry still left (like 999)
+        if (carry == 1) {
+            result.add(1);
+        }
+
+        Collections.reverse(result);
+        return result;
+    }
+}

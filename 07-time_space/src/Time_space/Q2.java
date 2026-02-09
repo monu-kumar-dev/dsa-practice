2c2e32464a66e5454b72f17f19d460a1cabd1c11
monu-kumar-dev <monukumar.software@gmail.com>
2026-02-09 17:48:46 +0530

    Add time and space complexity analysis for Q2

---
+++ 07-time_space/src/Time_space/Q2.java
@@ -0,0 +1,17 @@
+// Q. Find Time and Space Complexity.
+
+package Time_space;
+
+public class Q2 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i += 3) {
+            System.out.println("Hello");
+        }
+    }
+}
+
+// roughly loop runs n/3 times
+
+// Tc => O(n)
+// Sc => O(1)

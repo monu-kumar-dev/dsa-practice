0bb246169135067b912075953e343f80d944f376
monu-kumar-dev <monukumar.software@gmail.com>
2026-02-09 17:57:50 +0530

    feat: Q4 analyze time and space complexity of constant loop

---
+++ 07-time_space/src/Time_space/Q4.java
@@ -0,0 +1,14 @@
+// Q. Find Time and Space Complexity.
+
+package Time_space;
+
+public class Q4 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello");
+        }
+    }
+}
+
+// Tc => O(10) = O(1)
+// Sc => O(1)

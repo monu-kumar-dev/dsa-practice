0f2d39165f1c3ba5d6ca6883e00f09b264d6a89e
monu-kumar-dev <monukumar.software@gmail.com>
2026-02-09 20:57:06 +0530

    Analyze time and space complexity for loop with multiplicative factor 3

--- 07-time_space/src/Time_space/Q8.java
+++ 07-time_space/src/Time_space/Q8.java
@@ -2,6 +2,32 @@ package Time_space;
 
 public class Q8 {
     public static void main(String[] args) {
-        
+        int n = 10;
+
+        for (int i = 1; i < n; i *= 3) {
+            System.out.println("Hello");
+        }
     }
 }
+
+//  3^0          3^1          3^2           3^3   ........    3^K
+// i = 1        i = 3        i = 9         i = 27  .........   i = n
+//   1            1            1             1    .........     1
+
+// Solution:--->
+
+// 3^k = n
+// taking log both side
+// log(3^k) = log(n)
+// k log3 = logn
+// k = logn/log3
+// k = log3n
+
+/*
+ * Time Complexity:
+ * Total iterations = log3n
+ * TC = O(logn)
+ *
+ * Space Complexity:
+ * Only constant variables used -> O(1)
+ */

dddef4bb1967ff5f403a9ee449f645da922522b1
monu-kumar-dev <monukumar.software@gmail.com>
2026-03-02 00:24:18 +0530

    Refactor Selection Sort implementation for cleaner minIndex handling

--- 08-sorting/src/Sorting/SelectionSort.java
+++ 08-sorting/src/Sorting/SelectionSort.java
@@ -11,24 +11,41 @@ public class SelectionSort {
     public static void main(String[] args) {
         int arr[] = { 8, 4, 1, 9, -3, 6, 5 };
         print(arr);
+        // for (int i = 0; i < arr.length - 1; i++) {
+
+        // int min = Integer.MAX_VALUE;
+        // int minIndex = -1;
+        // for (int j = i; j < arr.length; j++) {
+        // if (arr[j] < min) {
+        // min = arr[j];
+        // minIndex = j;
+        // }
+        // }
+        // //   
+        // int temp = arr[i];
+        // arr[i] = arr[minIndex];
+        // arr[minIndex] = temp;
+        // }
+        // System.out.println();
+        // print(arr);
+
+        // Improve version:--->
+
         for (int i = 0; i < arr.length - 1; i++) {
 
-            int min = Integer.MAX_VALUE;
-            int minIndex = -1;
+            int minIndex = i;
             for (int j = i; j < arr.length; j++) {
-                if (arr[j] < min) {
-                    min = arr[j];
+                if (arr[j] < arr[minIndex]) {
                     minIndex = j;
                 }
             }
-            //
+            // swap
             int temp = arr[i];
             arr[i] = arr[minIndex];
             arr[minIndex] = temp;
         }
         System.out.println();
         print(arr);
-
     }
 
 }
